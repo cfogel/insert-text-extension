8018c8cb91dbb8a66fdcb7cde33b7aeb86f402fe
CJ Fogel <fogelcj@gmail.com>
2022-05-31 23:13:16 -0400

    add context menu option

---
+++ src/background.js
@@ -0,0 +1,50 @@
+import {phrases} from "./phrases.js";
+
+chrome.runtime.onInstalled.addListener(async () => {
+    for (const group of phrases.groups) {
+      chrome.contextMenus.create({
+        id: group.gid,
+        title: group.gtitle,
+        type: 'normal',
+        contexts: ['editable']
+      });
+
+      for (const pgroup of group.phrasegroups) {
+          let pparent = group.gid;
+          if (pgroup.pgid) {
+              chrome.contextMenus.create({
+                  id: pgroup.pgid,
+                  parentId: group.gid,
+                  title: pgroup.pgtitle,
+                  type: 'normal',
+                  contexts: ['editable']
+              })
+              pparent = pgroup.pgid;
+          }
+
+          for (const phrase of pgroup.phrases) {
+              chrome.contextMenus.create({
+                  id: phrase.pid,
+                  parentId: pparent,
+                  title: phrase.ptitle,
+                  type: 'normal',
+                  contexts: ['editable']
+              });
+
+              chrome.contextMenus.onClicked.addListener((info, tab) => {
+                  if (info.menuItemId == phrase.pid) {
+                      chrome.scripting.executeScript({
+                        target: { tabId: tab.id },
+                        func: addtext,
+                        args: [phrase.ptext]
+                      })
+                  }
+              });
+          }
+      }
+    }
+  });
+
+function addtext(t) {
+    document.activeElement.value = document.activeElement.value + t;
+}

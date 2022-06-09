fb21fdc1b623139f294d09b3175aae3c183f593f
CJ Fogel <fogelcj@gmail.com>
2022-06-09 08:34:08 -0400

    remove old listeners before adding new ones

--- src/background.js
+++ src/background.js
@@ -1,5 +1,7 @@
 import { phrases } from "./phrases.js";
 
+var events = Array();
+
 /** Use phrases.js as default when installed */
 chrome.runtime.onInstalled.addListener(async () => {
 
@@ -16,47 +18,54 @@ chrome.runtime.onInstalled.addListener(async () => {
  */
 function loadmenus(phraseobj) {
     for (const group of phraseobj.groups) {
-        chrome.contextMenus.create({
-            id: group.gid,
-            title: group.gtitle,
-            type: 'normal',
-            contexts: ['editable']
-        });
+        createCM(group.gid, null, group.gtitle);
 
         for (const pgroup of group.phrasegroups) {
-            let pparent = group.gid;
+            const pparent = (pgroup.pgid ? pgroup.pgid : group.gid);
             if (pgroup.pgid) {
-                chrome.contextMenus.create({
-                    id: pgroup.pgid,
-                    parentId: group.gid,
-                    title: pgroup.pgtitle,
-                    type: 'normal',
-                    contexts: ['editable']
-                });
-                pparent = pgroup.pgid;
+                createCM(pgroup.pgid, group.gid, pgroup.pgtitle);
             }
 
             for (const phrase of pgroup.phrases) {
-                chrome.contextMenus.create({
-                    id: phrase.pid,
-                    parentId: pparent,
-                    title: phrase.ptitle,
-                    type: 'normal',
-                    contexts: ['editable']
-                });
-
-                chrome.contextMenus.onClicked.addListener((info, tab) => {
-                    if (info.menuItemId == phrase.pid) {
-                        chrome.scripting.executeScript({
-                            target: { tabId: tab.id },
-                            func: addtext,
-                            args: [phrase.ptext],
-                        });
-                    }
-                });
+                createCM(phrase.pid, pparent, phrase.ptitle);
+                addContextMenuListener(phrase.pid, phrase.ptext);
             }
         }
     }
+}
+
+/**
+ * Create context menu with type 'normal' and context 'editable'
+ * @param {string} id Context menu id
+ * @param {string} parentId Context menu parentID
+ * @param {string} title Context menu title
+ */
+function createCM(id, parentId, title) {
+    chrome.contextMenus.create({
+        id,
+        parentId,
+        title,
+        type: 'normal',
+        contexts: ['editable']
+    });
+}
+
+/**
+ * Add context menu listener for phrase
+ * @param {string} pid Phrase ID
+ * @param {string} ptext Phrase text
+ */
+function addContextMenuListener(pid, ptext) {
+    let l = events.push((info, tab) => {
+        if (info.menuItemId == pid) {
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                func: addtext,
+                args: [ptext],
+            });
+        }
+    });
+    chrome.contextMenus.onClicked.addListener(events[l - 1]);
 }
 
 /**
@@ -84,6 +93,9 @@ chrome.runtime.onMessage.addListener(
 /** Update context menus when phrases change */
 chrome.storage.onChanged.addListener(() => {
     chrome.storage.local.get(["phrases"], async (result) => {
+        for (const e of events) {
+            chrome.contextMenus.onClicked.removeListener(e);
+        }
         chrome.contextMenus.removeAll();
         loadmenus(result.phrases);
     });

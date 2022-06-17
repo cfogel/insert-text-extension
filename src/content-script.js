fa2028c22c6bbf030eb3f40ed824c0e6a29d4ab0
CJ Fogel <fogelcj@gmail.com>
2022-06-17 03:30:22 -0400

    dispatch event to enable save button

--- src/content-script.js
+++ src/content-script.js
@@ -1,8 +1,9 @@
 if (document.body.childElementCount == 1) {
     chrome.runtime.onMessage.addListener((result, sender) => {
-        var tarea = document.querySelector(".PSPDFKit-Comment-Thread .PSPDFKit-Comment-Editor .PSPDFKit-Comment-Editor-Input");
+        var tarea = document.activeElement;
         if (tarea) {
             tarea.value = tarea.value + result.msg;
+            tarea.dispatchEvent(new Event('input', { bubbles: true }));
         }
 
     });
